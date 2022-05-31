be58e6fb9d8eb9650fa6ea9cee75465e0857c394
orviz <orviz@ifca.unican.es>
2022-05-31 13:53:58 +0200

    Add JePL file structure

--- .sqa/script.sleepy-puce-bobcat.sh
+++ .sqa/script.sleepy-puce-bobcat.sh
@@ -6,29 +6,26 @@ type = InfrastructureManager; username = %s; password = %q
 id = incd; type = OpenStack; host = https://stratus.ncg.ingrid.pt:5000; username = %s; password = %q; tenant = None; domain = default; auth_version = 3.x_password
 EOF
 if [ -z "$IM_USER" ] || [ -z "$IM_PASS" ] || [ -z "$OPENSTACK_USER" ] || [ -z "$OPENSTACK_PASS" ]; then
-  echo 'One or more credential variables are undefined'        
+  echo 'One or more credential variables are undefined (required: IM_USER, IM_PASS, OPENSTACK_USER, OPENSTACK_PASS)'
   exit 1
 fi
-echo "*********"
-cat /im/auth.dat
-echo "*********"
 printf "$(cat /im/auth.dat)" "${IM_USER}" "${IM_PASS}" "${OPENSTACK_USER}" "${OPENSTACK_PASS}" > /im/auth.dat
 echo "Generated auth.dat file:"
 ls -l /im/auth.dat
-printf "$(cat )" "https://stratus.ncg.ingrid.pt" "" > /im/test-ost.tosca
-echo "Printing tosca file"
-cat /im/test-ost.tosca
+printf "$(cat tosca.radl)" "https://stratus.ncg.ingrid.pt" "" > /im/test-ost.radl
+echo "Printing radl file"
+cat /im/test-ost.radl
 echo
-im_client.py -r "https://appsgrycap.i3m.upv.es:31443/im/" -a "/im/auth.dat" create_wait_outputs /im/test-ost.tosca > ./im_tosca.json
+im_client.py -r "https://appsgrycap.i3m.upv.es:31443/im/" -a "/im/auth.dat" create_wait_outputs /im/test-ost.radl > ./im_radl.json
 RETURN_CODE=$?
 echo "im_client.py create_wait_outputs return code: ${RETURN_CODE}"
 echo "Infrastructure Manager output:"
-cat ./im_tosca.json
-awk "/\{/,/\}/ { print $1 }" ./im_tosca.json > ./im_tosca_aux.json
+cat ./im_radl.json
+awk "/\{/,/\}/ { print $1 }" ./im_radl.json > ./im_radl_aux.json
 echo "Infrastructure Manager output (only json part):"
-cat ./im_tosca_aux.json
+cat ./im_radl_aux.json
 echo
-INFID=$(jq -r '[ .infid ] | .[]' ./im_tosca_aux.json)
+INFID=$(jq -r '[ .infid ] | .[]' ./im_radl_aux.json)
 echo "INFID=${INFID}"
 if [ ${RETURN_CODE} -eq 0 ] && ! [[ -z "${INFID}" && "x${INFID}x" == "xnullx" ]]; then
   echo "Deployment finished with success. Logs:"
