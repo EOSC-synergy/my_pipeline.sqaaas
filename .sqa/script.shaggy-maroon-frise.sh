c212d2db49fe6daaaf45d63d27339601e7d4487d
Pablo Orviz <orviz@ifca.unican.es>
2022-07-05 10:07:13 +0200

    EC3 implementation

--- .sqa/script.shaggy-maroon-frise.sh
+++ .sqa/script.shaggy-maroon-frise.sh
@@ -1,5 +1,4 @@
 (
-cp test.radl github.com/orviz/IM-sqaaas-test/test.radl
 mkdir /im
 cat <<EOF >> /im/auth.dat
 # InfrastructureManager auth
@@ -17,34 +16,8 @@ ls -l /im/auth.dat
 echo "*****************"
 cat /im/auth.dat
 echo "*****************"
-echo "Printing IM config file: github.com/orviz/IM-sqaaas-test/test.radl"
-cat github.com/orviz/IM-sqaaas-test/test.radl
-echo
-im_client.py -r "https://appsgrycap.i3m.upv.es:31443/im/" -a "/im/auth.dat" create_wait_outputs github.com/orviz/IM-sqaaas-test/test.radl > ./im_radl.json
-RETURN_CODE=$?
-echo "im_client.py create_wait_outputs return code: ${RETURN_CODE}"
-echo "Infrastructure Manager output:"
-cat ./im_radl.json
-awk "/\{/,/\}/ { print $1 }" ./im_radl.json > ./im_radl_aux.json
-echo "Infrastructure Manager output (only json part):"
-cat ./im_radl_aux.json
-echo
-INFID=$(jq -r '[ .infid ] | .[]' ./im_radl_aux.json)
-echo "INFID=${INFID}"
-if [ ${RETURN_CODE} -eq 0 ] && ! [[ -z "${INFID}" && "x${INFID}x" == "xnullx" ]]; then
-  echo "Deployment finished with success. Logs:"
-  im_client.py -r "https://appsgrycap.i3m.upv.es:31443/im/" -a "/im/auth.dat" getcontmsg ${INFID}
-  echo
-  im_client.py -r "https://appsgrycap.i3m.upv.es:31443/im/" -a "/im/auth.dat" destroy ${INFID}
-  echo "im_client.py destroy return code: $?"
-elif ! [[ -z "${INFID}" && "x${INFID}x" == "xnullx" ]]; then
-  echo "Deployment failed. Logs:"
-  im_client.py -r "https://appsgrycap.i3m.upv.es:31443/im/" -a "/im/auth.dat" getcontmsg ${INFID}
-  echo
-  im_client.py -r "https://appsgrycap.i3m.upv.es:31443/im/" -a "/im/auth.dat" destroy ${INFID}
-  echo "im_client.py destroy return code: $?"
-  exit ${RETURN_CODE}
-else
-  exit ${RETURN_CODE}
-fi
+
+ec3 launch jepl_im_cluster kubernetes ubuntu-ost -a "/im/auth.dat" -u https://appsgrycap.i3m.upv.es:31443/im/ -y
+ec3 show jepl_im_cluster -r
+ec3 destroy jepl_im_cluster --force -y
 )
